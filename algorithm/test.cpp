4015e9ffc407acfb3b365d4ad4506ca352ac2436
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-01-23 11:48:32 +0800

    fix: mac这么快就成功了

--- algorithm/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdint>
-
-int main() {
-    std::cout << "Size of int32_t: " << sizeof(int32_t) << " bytes" << std::endl;
-    std::cout << "Size of int64_t: " << sizeof(int64_t) << " bytes" << std::endl;
-
-    return 0;
-}
